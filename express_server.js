05aa284adc92d22cb6f3667af9b15cf7c48da325
BilliMorgan <shimko20@gmail.com>
2020-03-16 22:42:16 +0000

    added _header, urls_index, urls_show

--- express_server.js
+++ express_server.js
@@ -3,6 +3,9 @@ const app = express();
 const PORT = 8080;
 app.set("view engine", "ejs");
 
+
+
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -15,6 +18,10 @@ app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.get("/urls/:shortURL", (req,res) => {
+  let templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }; //need to check urlDatabase ???
+  res.render("urls_show", templateVars)
+})
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
