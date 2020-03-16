f97291040a042079a7e004c056cfec12127fd72e
BilliMorgan <shimko20@gmail.com>
2020-03-16 21:32:39 +0000

    added urls_index.ejs

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -10,7 +11,10 @@ const urlDatabase = {
 app.get("/",(req,res) => {
 res.send("Hello");
 });
-
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
