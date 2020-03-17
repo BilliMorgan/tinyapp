d536e827727dfbd4a42c2e2619f3fd96144c7cac
BilliMorgan <shimko20@gmail.com>
2020-03-17 16:45:43 +0000

    added links to shorkats, next stem is to change url to u

--- express_server.js
+++ express_server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser")
 
 const app = express();
 const PORT = 8080;
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 const randomNum = (min, max) => {
@@ -29,38 +29,60 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-app.post("/urls", (req,res) => {
-  console.log(req.body);
-  res.send("Ok")            //needs to replace this
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString();
+  const longURL = req.body.longURL
+  urlDatabase[shortURL] = longURL;
+
+  console.log(urlDatabase);
+  res.redirect(`/urls/${shortURL}`)            //needs to replace this
 })
-app.get("/",(req,res) => {
-res.send("Hello");
+app.get("/", (req, res) => {
+  res.send("Hello");
 });
+
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
-app.get("/urls/new", (req,res) => {                                                                //get  urls/new
+
+app.get("/urls/new", (req, res) => {                                                                //get  urls/new
   res.render("urls_new");
 });
-app.get("/urls/:shortURL", (req,res) => {
-  let templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }; //need to check urlDatabase ???
-  res.render("urls_show", templateVars)
-})
+
+app.get("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+  if (!longURL) {
+    res.statusCode = 404;
+    res.send(" Sorry, code 404, Page Not Found");
+  } else {
+    
+    // let templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  //  res.render("urls_show", templateVars)
+   res.redirect(longURL)
+  }
+  // //need to check urlDatabase ???
+  
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
-app.get("/hello", (req,res) => {
+
+app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n")
 });
-app.get("/set",(req,res) => {
+
+app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
-})
+});
 
 app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`)
-})
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
