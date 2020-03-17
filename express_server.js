b6272e63d7b98520ef95e4fdca3a6cc8b9a51c37
BilliMorgan <shimko20@gmail.com>
2020-03-17 00:07:02 +0000

    added form, post method and randomstring functiopn made byJay

--- express_server.js
+++ express_server.js
@@ -1,22 +1,47 @@
 const express = require("express");
+const bodyParser = require("body-parser")
+
 const app = express();
 const PORT = 8080;
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-
+const randomNum = (min, max) => {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+const generateRandomString = () => {
+  let randomKey = "";
+  for (let i = 0; i < 6; i++) {
+    const randomCase = randomNum(0, 3);
+    if (randomCase === 0) {
+      randomKey += randomNum(0, 9);
+    } else if (randomCase === 1) {
+      randomKey += String.fromCharCode(randomNum(65, 90));
+    } else {
+      randomKey += String.fromCharCode(randomNum(97, 122));
+    }
+  }
+  return randomKey;
+};
 
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+app.post("/urls", (req,res) => {
+  console.log(req.body);
+  res.send("Ok")            //needs to replace this
+})
 app.get("/",(req,res) => {
 res.send("Hello");
 });
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+app.get("/urls/new", (req,res) => {                                                                //get  urls/new
+  res.render("urls_new");
 });
 app.get("/urls/:shortURL", (req,res) => {
   let templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }; //need to check urlDatabase ???
